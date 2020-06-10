2860b589065cdbee23647db58da834d405da3c3a
Michael <mishumpel@mail.ru>
2020-06-10 14:08:01 +0300

    INIT

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    final String networkSSID = ("mySSID");
+    final String networkPassword = ("myPASS");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        try {
+            connectToWifi(networkSSID, "12345678");
+        } catch (Exception e) {
+        }
+        try {
+            connectToWifi(networkSSID, networkPassword);
+        } catch (Exception e) {
+        }
+    }
+
+    private void connectToWifi(final String networkSSID, final String networkPassword) {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = String.format("\"%s\"", networkSSID);
+        conf.preSharedKey = String.format("\"%s\"", networkPassword);
+
+        int netId = wifiManager.addNetwork(conf);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
+
+    }
+
+}
+
